81764b014c7a9c2f4331f66aeb0f99051a0c75af
Shubham Dev <shubhamdeogade97@gmail.com>
2024-01-23 21:22:16 +0530

    assign done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  // 2. Use a function to handle the button click and update the count
+  // const handleClick = () => {
+  //   setCount(count + 1);
+  // };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* 4. Use curly braces for dynamic content in JSX */}
+      <p>Button clicked {count} times</p>
+
+      {/* 3. Pass the handleClick function to the onClick event */}
+      {/* <button onClick={handleClick}>Click me</button> */}
+      
+      {/* 3. by using direct function inside onClick event */}
+      <button onClick={()=> setCount(count + 1)}>Click me</button>
     </div>
-  )
+  );
 }
 
 export default App
